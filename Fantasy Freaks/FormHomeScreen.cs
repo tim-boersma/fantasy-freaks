23a8c4c19e165180a41e27d54f90b872b10d0836
Leo Huynh <82899231+leohuynhDSU@users.noreply.github.com>
2022-11-06 15:08:08 -0600

    Adding Fantasy Freaks Application

---
+++ Fantasy Freaks/FormHomeScreen.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fantasy_Freaks {
+    public partial class FormHomeScreen : Form {
+        public static FormHomeScreen instance;
+
+        public FormHomeScreen() {
+            InitializeComponent();
+        }
+
+        private void FormHomeScreen_Load(object sender, EventArgs e) {
+            logo.Parent = titleBackground;
+            title.Parent = titleBackground;
+            FFWindow.instance.loadFont(this);
+        }
+
+        private void btnPlay_Click(object sender, EventArgs e) {
+            FFWindow.instance.changePanel(new FormSportSelection());
+        }
+    }
+
+}
