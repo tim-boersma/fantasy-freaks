a668bf230811914d1f8ca4e34e873fdc935a9d45
BruHaw <supbruhaw@gmail.com>
2022-11-17 15:43:43 -0600

    Fixed naming conventions for methods and parameters in winStat class

--- Fantasy Freaks/WinStat.cs
+++ Fantasy Freaks/WinStat.cs
@@ -20,42 +20,42 @@ namespace Fantasy_Freaks {
         public string DefName { get; set; }
         public int DefScore { get; set; }
 
-        public int eventDay() {
+        public int EventForDay() {
             Random rnd = new Random();
             int num = rnd.Next(0, 5);
             return num;
         }
 
-        public int Tpscore(int tp) {
-            tp -= 201;//lowest score
+        public int CalculateScoreFromTotalPoints(int PointsAllowed) {
+            PointsAllowed -= 201;//lowest score
             int score = 0;
-            while (tp >= 0) {
-                tp = tp - 20;//iteration = gap
-                if (tp >= 0)
+            while (PointsAllowed >= 0) {
+                PointsAllowed = PointsAllowed - 20;//iteration = gap
+                if (PointsAllowed >= 0)
                     score++;
             }
             return 48 - 2 * score;
         }
 
-        public static int TYscore(int ty) {
-            ty -= 4301;//lowest score
+        public static int CalculateScoreFromTotalYards(int TotalYards) {
+            TotalYards -= 4301;//lowest score
             int score = 0;
-            while (ty >= 0) {
-                ty = ty - 150;//iteration = gap
-                if (ty >= 0)
+            while (TotalYards >= 0) {
+                TotalYards = TotalYards - 150;//iteration = gap
+                if (TotalYards >= 0)
                     score++;
             }
             return 46 - 2 * score;
         }
 
-        public int defScoreCalc(int[] defense, int pointsAllowed, int yardsAllowed) {
+        public int CalculateDefensiveScore(int[] defense, int pointsAllowed, int yardsAllowed) {
             int defInt = 0, defFum = 0;
 
             return (defInt * 3) + (defFum * 2) + pointsAllowed + yardsAllowed;
         }
 
 
-        public double offScoreCalc(int[] offense) {
+        public double CalculateOffensiveScore(int[] offense) {
             int passTD = 0, rushTD = 0, recTD = 0, rec = 0, rushYRDs = 0, recYRDs = 0, passYRDs = 0, offInt = 0, offFum = 0;
 
             return (passTD * 4) + (rushTD * 6) + (recTD * 6) + (rec * 1) + (rushYRDs * .1) + (recYRDs * .1) + (passYRDs * .04) + (offInt * -3) + (offFum * -3);
