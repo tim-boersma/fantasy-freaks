5dc80d7c2b19e7d604b81085d4bbf294cc052b60
Tim Boersma <timothy.d.boersma@gmail.com>
2022-11-18 12:11:28 -0600

    branch switch commit

---
+++ Fantasy Freaks/ContributerService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.Json.Nodes;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Fantasy_Freaks
+{
+    public class ContributerService : IContributerService
+    {
+        public ContributerService(ApplicationDBContext context) { 
+            
+        }
+    }
+}
--- Fantasy Freaks/Program.cs
+++ Fantasy Freaks/Program.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,15 +10,34 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Fantasy_Freaks {
+
     internal static class Program {
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
+        /// 
+        public static IServiceProvider ServiceProvider { get; private set; }
+        public static IConfiguration Configuration { get; set; }
+
         [STAThread]
         static void Main() {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FFWindow());
+
+            var host = CreateHostBuilder().Build();
+            ServiceProvider = host.Services;
+
+            Application.Run(ServiceProvider.GetRequiredService<FFWindow>());
+        }
+
+        static IHostBuilder CreateHostBuilder()
+        {
+            return Host.CreateDefaultBuilder()
+                .ConfigureServices((context, services) => {
+                    //services.AddTransient<IHelloService, HelloService>();
+                    services.AddDbContext<ContributerDataContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+                    services.AddTransient<FFWindow>();
+                });
         }
     }
 }
