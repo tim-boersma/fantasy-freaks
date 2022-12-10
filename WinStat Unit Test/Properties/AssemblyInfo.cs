989ad9598617c618b6ccd6a3675ec0ff59f43265
CMaynard1 <cameronj.maynard@gmail.com>
2022-12-09 21:54:54 -0600

    Finished unit testing for methods in WinStat and confirmed they were also successful.

---
+++ WinStat Unit Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WinStat Unit Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WinStat Unit Test")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("29649032-3779-435c-ad0b-89c682ac1bec")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ WinStat Unit Test/UnitTest1.cs
@@ -0,0 +1,56 @@
+﻿using Fantasy_Freaks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace WinStat_Unit_Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestPoints()
+        {
+            int result, PointsAllowed, actual;
+            int score;
+            for (int i = 100; i <= 1000; i += 50)
+            {
+                result = Fantasy_Freaks.WinStat.CalculateScoreFromTotalPoints(i);
+
+                PointsAllowed = i - 201;
+                score = 0;
+                while (PointsAllowed >= 0)
+                {
+                    PointsAllowed = PointsAllowed - 20;
+                    if (PointsAllowed >= 0)
+                        score++;
+                }
+                actual = 48 - 2 * score;
+
+                Assert.IsTrue(result == actual);
+            }
+        }
+
+        [TestMethod]
+        public void TestYards()
+        {
+            int result, TotalYards, actual;
+            int score;
+            for (int i = 100; i <= 1000; i += 50)
+            {
+                result = Fantasy_Freaks.WinStat.CalculateScoreFromTotalYards(i);
+
+                TotalYards = i - 4301;
+                score = 0;
+                while (TotalYards >= 0)
+                {
+                    TotalYards = TotalYards - 150;
+                    if (TotalYards >= 0)
+                        score++;
+                }
+                actual = 46 - 2 * score;
+
+                Assert.IsTrue(result == actual);
+            }
+        }
+    }
+}
